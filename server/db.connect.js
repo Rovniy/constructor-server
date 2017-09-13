a1d3c8a940df768938bd65ee63f005b0ea8c8ec5
Ravy <smart.fz@mail.ru>
2017-09-13 11:35:04 +0300

    Mongo functions

--- server/db.connect.js
+++ server/db.connect.js
@@ -7,26 +7,56 @@ var config = {
     url: 'mongodb://localhost:27017',
     dbName: 'uspy'
 },
-mongoClient = require("mongodb").MongoClient;
+    db = undefined,
+    mongoClient = require("mongodb").MongoClient;
 
-mongoClient.connect(config.url + '/' + config.dbName, function(err, db){
-    if (db) console.log('DB-SERVER: Mongodb activated. Address: ' + config.url + '. Database: ' + config.dbName);
-    //getItem(db); //Получение списка элементов и поиск по элементам
-    
-    db.close();
+mongoClient.connect(config.url + '/' + config.dbName, function(err, database){
+    db = database;
+    if (db) {
+        console.log('DB-SERVER: Mongodb activated. Address: ' + config.url + '. Database: ' + config.dbName);
+        setSingleItem('Users', {name:'Ravy'});
+        
+        var ravy = getSingleItem('Users', {name:'Ravy'});
+        console.log('Ravy',ravy);
+
+        var all = getAllItemsToArray('Users');
+        console.log('all',all);
+
+        db.close();
+    } else {
+        console.log('DB-SERVER: CONNECTION FAILURE!');
+    }
 });
 
 /**
- * Получение списка элементов и поиск по элементам
- * @param db
+ * Получение любого элемента
+ * @param collection - name of collection from need extract
+ * @param obj - Object with needed key : value that find
  */
-function getItem(db) {
-    db.collection('Users', function(err, collection) {
-        collection.find({'name':'Ravy'}, function(err, cursor) {
+function getSingleItem(collection, obj) {
+    db.collection(collection, function(err, collection) {
+        collection.find(obj, function(err, cursor) {
             // Преобразовываем в массив
-            cursor.toArray(function(err, items) {
-                console.log(items)
-            });
+            return cursor;
         });
     });
+}
+
+/**
+ * Insert single object into collection
+ * @param collection - Collection name
+ * @param obj - { key : value } - Object that need to insert
+ */
+function setSingleItem(collection, obj) {
+    db.collection(collection).insertOne(obj, function(err, result) {
+        console.log("Inserted into:",collection,', data:',result.ops);
+    });
+}
+
+function getAllItemsToArray(collection) {
+    return function() {
+        db.collection(collection).find({}).toArray(function(err, result){
+            return result;
+        });
+    }
 }
